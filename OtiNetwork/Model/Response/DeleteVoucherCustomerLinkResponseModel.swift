0391978e16c8ff9406c9fc767b16ef8560d913c8
Cüneyt AYVAZ <cnyt.ayvaz@gmail.com>
2019-10-11 08:21:02 +0300

    DeleteVoucherCustomerLinkResponseModel updated

--- OtiNetwork/Model/Response/DeleteVoucherCustomerLinkResponseModel.swift
+++ OtiNetwork/Model/Response/DeleteVoucherCustomerLinkResponseModel.swift
@@ -10,18 +10,21 @@ import ObjectMapper
 
 public class DeleteVoucherCustomerLinkResponseModel: Mappable {
     
-    public var success:Bool!
+    public var success: Bool!
+    public var message: String!
     
     public required init?(map: Map) {
         
     }
     
-    public init(success:Bool) {
+    public init(success: Bool, message: String) {
         self.success = success
+        self.message = message
     }
     
     public func mapping(map: Map) {
         success <- map["Success"]
+        message <- map["Message"]
         
     }
     

c929ab3f02881062011c299eb53019b9435982f7
Cüneyt AYVAZ <cnyt.ayvaz@gmail.com>
2020-02-10 14:28:14 +0300

    GetSaledExcursionInfoRequestModel updated

--- OtiNetwork/Model/Request/GetSaledExcursionInfoRequestModel.swift
+++ OtiNetwork/Model/Request/GetSaledExcursionInfoRequestModel.swift
@@ -13,19 +13,22 @@ public class GetSaledExcursionInfoRequestModel: Mappable {
     
     public var incomingId: Int?
     public var touristId: Int?
+    public var customerId: Int?
     
     public required init?(map: Map) {
         
     }
     
-    public init(incomingId: Int?, touristId: Int?) {
+    public init(incomingId: Int?, touristId: Int?, customerId: Int?) {
         self.incomingId = incomingId
         self.touristId = touristId
+        self.customerId = customerId
     }
     
     public func mapping(map: Map) {
         incomingId <- map["IncomingId"]
         touristId <- map["TouristId"]
+        customerId <- map["CustomerId"]
     }
     
 }

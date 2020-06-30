1a64d078bf5bb5940567d0cb4605c3423e6eba3d
Akif Demirezen <maddemirezen@gmail.com>
2020-06-30 23:41:18 +0300

    pushNotificationPermission eklendi.

--- OtiNetwork/Model/Request/CustomerControlActivateCodeRequestModel.swift
+++ OtiNetwork/Model/Request/CustomerControlActivateCodeRequestModel.swift
@@ -14,20 +14,23 @@ public class CustomerControlActivateCodeRequestModel: Mappable {
     public var mobilePhone:String!
     public var activeCode:String!
     public var email:String!
+    public var pushNotificationPermission: Bool!
     
     public required init?(map: Map) {
         
     }
     
-    public init(mobilePhone:String,activeCode : String,email: String) {
+    public init(mobilePhone:String,activeCode : String,email: String, pushNotificationPermission: Bool = true) {
         self.mobilePhone = mobilePhone
         self.activeCode = activeCode
         self.email = email
+        self.pushNotificationPermission = pushNotificationPermission
     }
     
     public func mapping(map: Map) {
         mobilePhone <- map["MobilePhone"]
         activeCode <- map["ActiveCode"]
         email <- map["Email"]
+        pushNotificationPermission <- map["PushNotificationPermission"]
     }
 }

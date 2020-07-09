2eb13d7bd6ecb7a79ece9da0d03acb0345f02a75
Akif Demirezen <maddemirezen@gmail.com>
2020-07-09 23:17:28 +0300

    email - sms permission eklendi.

--- OtiNetwork/Model/Request/CustomerControlActivateCodeRequestModel.swift
+++ OtiNetwork/Model/Request/CustomerControlActivateCodeRequestModel.swift
@@ -15,16 +15,20 @@ public class CustomerControlActivateCodeRequestModel: Mappable {
     public var activeCode:String!
     public var email:String!
     public var pushNotificationPermission: Bool!
+    public var emailPermission: Bool!
+    public var smsPermission: Bool!
     
     public required init?(map: Map) {
         
     }
     
-    public init(mobilePhone:String,activeCode : String,email: String, pushNotificationPermission: Bool = true) {
+    public init(mobilePhone:String,activeCode : String,email: String, pushNotificationPermission: Bool = true, emailPermission: Bool = true, smsPermission: Bool = true) {
         self.mobilePhone = mobilePhone
         self.activeCode = activeCode
         self.email = email
         self.pushNotificationPermission = pushNotificationPermission
+        self.emailPermission = emailPermission
+        self.smsPermission = smsPermission
     }
     
     public func mapping(map: Map) {
@@ -32,5 +36,7 @@ public class CustomerControlActivateCodeRequestModel: Mappable {
         activeCode <- map["ActiveCode"]
         email <- map["Email"]
         pushNotificationPermission <- map["PushNotificationPermission"]
+        emailPermission <- map["EmailPermission"]
+        smsPermission <- map["SmsPermission"]
     }
 }

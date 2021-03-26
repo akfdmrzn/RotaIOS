4f8f51da50599acf132b6dd95123247f7c030aec
Akif Demirezen <maddemirezen@gmail.com>
2021-03-26 20:39:09 +0300

    added is edit

--- OtiNetwork/Model/Request/GetSuitcaseRequestModel.swift
+++ OtiNetwork/Model/Request/GetSuitcaseRequestModel.swift
@@ -12,19 +12,22 @@ public class GetSuitcaseRequestModel: Mappable{
     
     public var customerId:Int!
     public var tourId:String!
+    public var isEdit:Bool!
     
     public required init?(map: Map) {
         
     }
     
-    public init(customerId: Int, tourId: String) {
+    public init(customerId: Int, tourId: String,isEdit : Bool) {
         self.customerId = customerId
         self.tourId = tourId
+        self.isEdit = isEdit
     }
     
     public func mapping(map: Map) {
         customerId <- map["CustomerId"]
         tourId <- map["TourId"]
+        tourId <- map["IsEdit"]
     }
     
 }

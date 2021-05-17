83ccdc607ae981b799058e0321ef7109daec8978
Akif Demirezen <maddemirezen@gmail.com>
2021-05-17 13:06:59 +0300

    update search response model

--- OtiNetwork.podspec
+++ OtiNetwork.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
     s.name             = 'OtiNetwork'
-    s.version          = '1.4.2'
+    s.version          = '1.4.3'
     s.summary          = 'OtiNetworking Manager includes configuration private'
     
     # This description is used to generate tags and improve search results.
--- OtiNetwork/Model/Response/GetSearchResponseModel.swift
+++ OtiNetwork/Model/Response/GetSearchResponseModel.swift
@@ -11,19 +11,37 @@ import ObjectMapper
 
 public class GetSearchResponseModel: Mappable {
     
-    public var url: String!
+    public var id_: Int!
+    public var day: String!
+    public var name: String!
+    public var dayString: String!
+    public var priceType: String!
+    public var price: Double!
+    public var priceCurrency: String!
+    public var imageUrl: String!
+    public var isRecommended: Bool!
+    public var isNew: Bool!
+    public var isBestSeller: Bool!
+    public var passportRequired: Bool!
+    public var isVendable: Bool!
     
     public required init?(map: Map) {
         
     }
     
-    public init(url: String) {
-        self.url = url
-    }
-    
     public func mapping(map: Map) {
-        url <- map["Url"]
+        id_ <- map["Id"]
+        day <- map["Day"]
+        name <- map["Name"]
+        dayString <- map["DayString"]
+        priceType <- map["PriceType"]
+        priceCurrency <- map["PriceCurrency"]
+        imageUrl <- map["ImageUrl"]
+        isRecommended <- map["IsRecommended"]
+        isNew <- map["IsNew"]
+        isBestSeller <- map["IsBestSeller"]
+        passportRequired <- map["PassportRequired"]
+        isVendable <- map["IsVendable"]
         
     }
-    
 }

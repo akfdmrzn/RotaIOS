5b0cb260aa1d99b6ead685012a7904577f59c83b
betul <betul.odeon@gmail.com>
2021-06-21 15:42:25 +0300

    updated ReceivePaymentResponseModel.

--- OtiNetwork/Model/Response/ReceivePaymentResponseModel.swift
+++ OtiNetwork/Model/Response/ReceivePaymentResponseModel.swift
@@ -8,7 +8,7 @@
 import UIKit
 import ObjectMapper
 
-public class ReceivePaymentResponseModel: NSObject {
+public class ReceivePaymentResponseModel: Mappable {
     
     public var url: String!
     

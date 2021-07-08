7d14ec9629f333cad43ec9da6762926ea324a9fd
Akif Demirezen <maddemirezen@gmail.com>
2021-07-08 11:50:36 +0300

    bug fix receive payment

--- OtiNetwork/Model/Request/ReceivePaymentRequestModel.swift
+++ OtiNetwork/Model/Request/ReceivePaymentRequestModel.swift
@@ -11,40 +11,86 @@ import ObjectMapper
 public class ReceivePaymentRequestModel: Mappable{
     
     public var userId : Int?
-    public var excursionId : Int?
+    public var payId : Int?
+    public var card : ReceiveCardModel?
+    public var excursion : ReceiveExcursionModel?
+    public var calculateTotalPrice : CalculateTotalPriceResponseModel?
+    
+    public required init?(map: Map) {
+        
+    }
+    
+    public init(userId: Int, payId: Int,card : ReceiveCardModel,excursion : ReceiveExcursionModel,calculateTotalPrice : CalculateTotalPriceResponseModel) {
+        self.userId = userId
+        self.payId = payId
+        self.card = card
+        self.excursion = excursion
+        self.calculateTotalPrice = calculateTotalPrice
+    }
+
+    public func mapping(map: Map) {
+        userId <- map["UserId"]
+        payId <- map["payId"]
+        card <- map["card"]
+        excursion <- map["excursion"]
+        calculateTotalPrice <- map["calculateTotalPrice"]
+    }
+}
+
+public class ReceiveCardModel: Mappable{
+    
     public var creditCard : String?
     public var cvv : String?
     public var owner : String?
     public var cardMonth : String?
     public var cardYear : String?
     public var currencyCode : String?
-    public var amount : Double?
     
     public required init?(map: Map) {
         
     }
     
-    public init(userId: Int, excursionId: Int, creditCard: String, cvv: String, owner: String, cardMonth: String, cardYear: String, currencyCode: String, amount: Double) {
-        self.userId = userId
-        self.excursionId = excursionId
+    public init(creditCard: String,cvv : String,owner : String,cardMonth : String,cardYear : String,currencyCode : String) {
         self.creditCard = creditCard
         self.cvv = cvv
         self.owner = owner
         self.cardMonth = cardMonth
         self.cardYear = cardYear
         self.currencyCode = currencyCode
-        self.amount = amount
     }
 
     public func mapping(map: Map) {
-        userId <- map["UserId"]
-        excursionId <- map["ExcursionId"]
         creditCard <- map["CreditCard"]
         cvv <- map["Cvv"]
         owner <- map["Owner"]
         cardMonth <- map["CardMonth"]
         cardYear <- map["CardYear"]
         currencyCode <- map["CurrencyCode"]
-        amount <- map["Amount"]
+    }
+}
+
+public class ReceiveExcursionModel: Mappable{
+    
+    public var passaportId : [Int]?
+    public var totalAmount : Double?
+    public var checkOutDate : String?
+    public var arrivalFlightDate : String?
+    
+    public required init?(map: Map) {
+        
+    }
+    
+    public init(passaportId: [Int],totalAmount : Double,checkOutDate : String,arrivalFlightDate : String) {
+        self.passaportId = passaportId
+        self.totalAmount = totalAmount
+        self.checkOutDate = checkOutDate
+        self.arrivalFlightDate = arrivalFlightDate
+    }
+
+    public func mapping(map: Map) {
+        passaportId <- map["pasaportId"]
+        totalAmount <- map["totalAmount"]
+        checkOutDate <- map["checkoutDate"]
+        arrivalFlightDate <- map["arrivalFlightDate"]
     }
 }
--- OtiNetwork/Model/Response/ReceivePaymentResponseModel.swift
+++ OtiNetwork/Model/Response/ReceivePaymentResponseModel.swift
@@ -11,17 +11,19 @@ import ObjectMapper
 public class ReceivePaymentResponseModel: Mappable {
     
     public var url: String!
+    public var threadGuid: String!
     
     public required init?(map: Map) {
         
     }
     
-    public init(url: String) {
+    public init(url: String,threadGuid : String) {
         self.url = url
+        self.threadGuid = threadGuid
     }
     
     public func mapping(map: Map) {
         url <- map["Url"]
-        
+        threadGuid <- map["ThreadGuid"]
     }
 }

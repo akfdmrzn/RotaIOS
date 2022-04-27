16d63feb9aadd57c8acd0e277421e5e87ca32235
Yasin <ysndlklc1989@icloud.com>
2022-04-27 09:36:27 +0300

    ROTAM-378

--- RotaIOS/Controller/OnlineSalesDetailView.swift
+++ RotaIOS/Controller/OnlineSalesDetailView.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Foundation
+import UIKit
+
+final class OnlineSalesDetailView : UIView {
+    @IBOutlet weak var viewHeaderView: HeaderDetailCustomView!
+    @IBOutlet weak var viewContentView: UIView!
+    
+override func awakeFromNib() {
+    self.viewHeaderView.labelHeaderDetailView.text = "Online Sales"
+    self.viewContentView.backgroundColor = UIColor.tableViewColor
+    self.viewContentView.layer.cornerRadius = 10
+
+}
+
+required init(customParamArg: String) {
+    super.init(frame: .zero)
+}
+
+required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+}
+}
